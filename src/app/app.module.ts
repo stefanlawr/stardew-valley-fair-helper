f4ed9873bb24cf3e82120a8f0c52e6992e2d409d
Stefan S <stefanlawr@gmail.com>
2019-08-13 15:33:37 -0400

    Added header; Added cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FairHelperComponent } from './fair-helper/fair-helper.component';
+import { NavComponent } from './nav/nav.component';
+import { MaterialModule } from 'src/material-module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FairHelperComponent
+    FairHelperComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
