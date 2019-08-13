2864754cfba051a5aef2ff13921d5b9f5cb09447
Stefan S <stefanlawr@gmail.com>
2019-08-13 19:08:08 -0400

    Made Material File Input

--- src/app/fair-helper/fair-helper.component.ts
+++ src/app/fair-helper/fair-helper.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fair-helper.component.scss']
 })
 export class FairHelperComponent implements OnInit {
+  srcResult: any;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  /**
+   *  Allows for Angular Material button to accept inputs of type file
+   */
+  onFileSelected() {
+    const inputNode: any = document.querySelector('#file');
+
+    if (typeof (FileReader) !== 'undefined') {
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        this.srcResult = e.target.result;
+      };
+
+      reader.readAsArrayBuffer(inputNode.files[0]);
+    }
   }
 
 }
