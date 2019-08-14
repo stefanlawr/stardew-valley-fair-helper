601a52c5fcb2415dc84fdc495879835d0368a0d1
Strauch Stefan (TFX4WDP) <TFX4WDP@UPS.COM>
2019-08-14 10:59:40 -0400

    Item drop down populated

--- src/app/fair-helper/fair-helper.component.ts
+++ src/app/fair-helper/fair-helper.component.ts
@@ -1,24 +1,39 @@
-import { Star } from './fair-helper.model';
+import { Star, Category, Items, Info } from './fair-helper.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-fair-helper',
   templateUrl: './fair-helper.component.html',
-  styleUrls: ['./fair-helper.component.scss']
+  styleUrls: ['./fair-helper.component.scss'],
 })
 export class FairHelperComponent implements OnInit {
   srcResult: any;
 
+  ITEMS = Items;
+  INFO = Info;
+
   STARS: Star[] = [
-    {rarity: 0, text: 'No Star'},
-    {rarity: 1, text: 'Silver'},
-    {rarity: 2, text: 'Gold'},
-    {rarity: 3, text: 'Iridium'}
+    { rarity: 0, text: 'No Star' },
+    { rarity: 1, text: 'Silver' },
+    { rarity: 2, text: 'Gold' },
+    { rarity: 3, text: 'Iridium' },
   ];
 
-  constructor() { }
+  CATEGORIES: Category[] = [
+    { text: 'Animal Products' },
+    { text: 'Artisan Goods' },
+    { text: 'Cooked Items' },
+    { text: 'Fish' },
+    { text: 'Forage & Flowers' },
+    { text: 'Fruits' },
+    { text: 'Gems & Minerals' },
+    { text: 'Vegetables' },
+  ];
+
+  constructor() {}
 
   ngOnInit() {
+    console.log(this.ITEMS[1].children[1].text);
   }
 
   /**
@@ -27,7 +42,7 @@ export class FairHelperComponent implements OnInit {
   onFileSelected() {
     const inputNode: any = document.querySelector('#file');
 
-    if (typeof (FileReader) !== 'undefined') {
+    if (typeof FileReader !== 'undefined') {
       const reader = new FileReader();
       reader.readAsText(inputNode.files[0]);
 
